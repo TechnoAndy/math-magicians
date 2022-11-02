6fa80570f4c29dbaf4b6e753b687b48a660b1dcf
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-02 19:21:49 +0200

    Remove eslint disables and fix errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/self-closing-comp */
-
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
@@ -15,7 +12,7 @@ function Calculator() {
     <div className="App">
       <div className="calc-container">
         <div className="display">
-          <span></span>
+          <span />
           {' '}
           { total }
           { operation }
@@ -23,29 +20,29 @@ function Calculator() {
         </div>
 
         <div className="digits">
-          <button className="press-btn" onClick={btnClick}>AC</button>
-          <button className="press-btn" onClick={btnClick}>+/-</button>
-          <button className="press-btn" onClick={btnClick}>%</button>
-          <button className="press-btn amber" onClick={btnClick}>÷</button>
+          <button type="button" className="press-btn" onClick={btnClick}>AC</button>
+          <button type="button" className="press-btn" onClick={btnClick}>+/-</button>
+          <button type="button" className="press-btn" onClick={btnClick}>%</button>
+          <button type="button" className="press-btn amber" onClick={btnClick}>÷</button>
 
-          <button className="press-btn-1" onClick={btnClick}>7</button>
-          <button className="press-btn-1" onClick={btnClick}>8</button>
-          <button className="press-btn-1" onClick={btnClick}>9</button>
-          <button className="press-btn amber" onClick={btnClick}>x</button>
+          <button type="button" className="press-btn-1" onClick={btnClick}>7</button>
+          <button type="button" className="press-btn-1" onClick={btnClick}>8</button>
+          <button type="button" className="press-btn-1" onClick={btnClick}>9</button>
+          <button type="button" className="press-btn amber" onClick={btnClick}>x</button>
 
-          <button className="press-btn" onClick={btnClick}>4</button>
-          <button className="press-btn" onClick={btnClick}>5</button>
-          <button className="press-btn" onClick={btnClick}>6</button>
-          <button className="press-btn amber" onClick={btnClick}>-</button>
+          <button type="button" className="press-btn" onClick={btnClick}>4</button>
+          <button type="button" className="press-btn" onClick={btnClick}>5</button>
+          <button type="button" className="press-btn" onClick={btnClick}>6</button>
+          <button type="button" className="press-btn amber" onClick={btnClick}>-</button>
 
-          <button className="press-btn" onClick={btnClick}>1</button>
-          <button className="press-btn" onClick={btnClick}>2</button>
-          <button className="press-btn" onClick={btnClick}>3</button>
-          <button className="press-btn amber" onClick={btnClick}>+</button>
+          <button type="button" className="press-btn" onClick={btnClick}>1</button>
+          <button type="button" className="press-btn" onClick={btnClick}>2</button>
+          <button type="button" className="press-btn" onClick={btnClick}>3</button>
+          <button type="button" className="press-btn amber" onClick={btnClick}>+</button>
 
-          <button className="press-btn-2" onClick={btnClick}>0</button>
-          <button className="press-btn" onClick={btnClick}>.</button>
-          <button className="press-btn amber" onClick={btnClick}>=</button>
+          <button type="button" className="press-btn-2" onClick={btnClick}>0</button>
+          <button type="button" className="press-btn" onClick={btnClick}>.</button>
+          <button type="button" className="press-btn amber" onClick={btnClick}>=</button>
         </div>
       </div>
     </div>
