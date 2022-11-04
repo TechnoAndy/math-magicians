1dce9bc6d9236c57b17cb4764f29a29e4f2f7e47
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-04 18:04:23 +0200

    Fixed some styling and errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 function Calc() {
-  const [state, setState] = useState({ total: 0 });
+  const [position, setposition] = useState({ total: 0 });
   function clkBtn(e) {
-    setState((state) => calculate(state, e.target.innerText));
+    setposition((state) => calculate(state, e.target.innerText));
   }
 
-  const { total, next, operation } = state;
+  const { total, next, operation } = position;
   return (
     <div className="App">
-      <div className="ccontainer">
+      <div className="c-container">
         <div className="display">
           <span />
           {' '}
@@ -23,30 +23,30 @@ function Calc() {
           <button type="button" className="press-btn" onClick={clkBtn}>AC</button>
           <button type="button" className="press-btn" onClick={clkBtn}>+/-</button>
           <button type="button" className="press-btn" onClick={clkBtn}>%</button>
-          
+
           <button type="button" className="press-btn amber" onClick={clkBtn}>÷</button>
 
           <button type="button" className="press-btn-1" onClick={clkBtn}>7</button>
           <button type="button" className="press-btn-1" onClick={clkBtn}>8</button>
           <button type="button" className="press-btn-1" onClick={clkBtn}>9</button>
-          
+
           <button type="button" className="press-btn amber" onClick={clkBtn}>x</button>
 
           <button type="button" className="press-btn" onClick={clkBtn}>4</button>
           <button type="button" className="press-btn" onClick={clkBtn}>5</button>
           <button type="button" className="press-btn" onClick={clkBtn}>6</button>
-          
+
           <button type="button" className="press-btn amber" onClick={clkBtn}>-</button>
 
           <button type="button" className="press-btn" onClick={clkBtn}>1</button>
           <button type="button" className="press-btn" onClick={clkBtn}>2</button>
           <button type="button" className="press-btn" onClick={clkBtn}>3</button>
-          
+
           <button type="button" className="press-btn amber" onClick={clkBtn}>+</button>
 
-          <button type="button" className="press-btn-2" onClick={clkBtn}>0</button>
+          <button type="button" className="press-btn-0" onClick={clkBtn}>0</button>
           <button type="button" className="press-btn" onClick={clkBtn}>.</button>
-          
+
           <button type="button" className="press-btn amber" onClick={clkBtn}>=</button>
         </div>
       </div>
