48827feef2156dd15e97172db07c7963465b9bdb
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-08 20:57:47 +0200

    Setup test folders and files

---
+++ src/components/_tests_/Calculator.test.js
@@ -0,0 +1,3 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import Calc from '../Calculator';
---
+++ src/components/_tests_/Navbar.test.js
@@ -0,0 +1,6 @@
+import React from  'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Nav from '../Navbar';
