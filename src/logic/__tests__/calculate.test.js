6ad77d756cd7337938a86f051595ae5ba9bb83af
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-09 17:22:12 +0200

    Update calculate.test.js

--- src/logic/__tests__/calculate.test.js
+++ src/logic/__tests__/calculate.test.js
@@ -1,7 +1,7 @@
 import calculate from '../calculate';
 
 describe('Testing calculate functionality', () => {
-  test('must return total', () => {
+  test('must return answer', () => {
     expect(calculate({ total: '15', next: '4', operation: '+' }, '=')).toEqual({ total: '19', next: null, operation: null });
     expect(calculate({ total: '10', next: '2', operation: '-' }, '=')).toEqual({ total: '8', next: null, operation: null });
     expect(calculate({ total: '15', next: '4', operation: 'x' }, '=')).toEqual({ total: '60', next: null, operation: null });
