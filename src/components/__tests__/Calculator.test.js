2885f06fc290b43f3cbed4103f85b7c87c83a4d8
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-11-09 19:00:55 +0300

    Added calculator test

---
+++ src/components/__tests__/Calculator.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Calc from '../Calculator';
+
+describe('Test for Calculator', () => {
+  test('should render Calculator ', () => {
+    render(<Calc />);
+    expect(screen).toMatchSnapshot();
+  });
+});
