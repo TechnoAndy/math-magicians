9079d3243ad775048d49825f19f44666bbc8dd55
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-11-09 19:01:15 +0300

    Added home test

--- src/components/__tests__/Home.test.js
+++ src/components/__tests__/Home.test.js
@@ -1,1 +1,9 @@
 
+import { render } from '@testing-library/react';
+import React from 'react';
+import Home from '../Home';
+
+it('Home renders as expected', () => {
+  const Task = render(<Home />);
+  expect(Task).toMatchSnapshot();
+});
